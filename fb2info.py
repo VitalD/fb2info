4261c29337ebba6944a9e984636a5c6cd1f35b4f
VitalD <vkontakte.vit@gmail.com>
2014-02-28 00:24:48 +0300

    Update fb2info.py

--- fb2info.py
+++ fb2info.py
@@ -5,16 +5,21 @@ import base64
 import xml.etree.ElementTree as ET
 import Image
 from StringIO import StringIO
+import urllib
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 2:
 	print('Not enough actual parameters')
 	sys.exit(1)
 
 inputFile = sys.argv[1]
 outputFile = sys.argv[2]
-size = int(sys.argv[3])
+if len(sys.argv) > 2:
+	size = int(sys.argv[3])
+else:
+	size = 256
 
-root = ET.parse(inputFile).getroot()
+iFile = open(urllib.url2pathname(inputFile).split('file://')[1],'r')
+root = ET.parse(iFile).getroot()
 
 for i in root.iter():
 	if (i.tag.split('}')[1] == 'binary') and ('id' in i.attrib) and (i.attrib['id'].split('.')[0] == 'cover'):
