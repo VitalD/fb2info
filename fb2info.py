79b88ba297ce9c8475e222f638977af58182437b
VitalD <vkontakte.vit@gmail.com>
2014-02-27 22:02:47 +0300

    Update fb2info.py
    Added resizing and output in png.

--- fb2info.py
+++ fb2info.py
@@ -3,23 +3,26 @@
 import sys
 import base64
 import xml.etree.ElementTree as ET
+import Image
+from StringIO import StringIO
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
 	print('Not enough actual parameters')
 	sys.exit(1)
 
 inputFile = sys.argv[1]
 outputFile = sys.argv[2]
+size = sys.argv[3]
 
-tree = ET.parse(inputFile)
-root = tree.getroot()
-
-cover_raw = None
+root = ET.parse(inputFile).getroot()
 
 for i in root.iter():
 	if (i.tag.split('}')[1] == 'binary') and ('id' in i.attrib) and (i.attrib['id'].split('.')[0] == 'cover'):
 		cover_raw = i.text
-		open(outputFile,'wb').write(base64.decodestring(cover_raw))
+		cover_decoded = base64.decodestring(cover_raw)
+		cover = Image.open(StringIO(cover_decoded))
+		cover.thumbnail((size,size), Image.ANTIALIAS)
+		cover.save(outputFile,"PNG")
 		sys.exit(0)
 
 print('No cover inside')
